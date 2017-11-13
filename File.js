c4c04fe5527121669fb4a0905cd50cf5a33c6a5c
OrangeM21 <orangemtony@gmail.com>
2017-11-14 01:21:54 +0800

    add

--- File.js
+++ File.js
@@ -1,11 +1,9 @@
-var name = "The Window";
-　　var object = {
-　　　　name : "My Object",
-　　　　getNameFunc : function(){
-　　　　　　var that = this;
-　　　　　　return function(){
-　　　　　　　　return that.name;
-　　　　　　};
-　　　　}
-　　};
-　　console.log(object.getNameFunc()());
+const Koa = require('koa');
+
+const app = new Koa();
+
+const main = ctx => {
+    ctx.response.body = "Hello World"
+};
+app.use(main);
+app.listen(3000);
